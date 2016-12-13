03c7f23d4eae0e2c93b994eb9cf7876d9a2f1124
haerinpc <haerinpc@hrin>
2016-12-13 20:41:37 +0900

    add gugudan

--- test/src/main/java/org/zerock/web/GuGudan.java
+++ test/src/main/java/org/zerock/web/GuGudan.java
@@ -4,5 +4,13 @@ public class GuGudan {
 
 	public static void main(String[] args) {
 		
+		for(int i = 2 ; i < 10 ; i++){
+			for(int j = 1 ; j < 10 ; j++){
+				System.out.print(i + " x " + j +" = " + (i*j) + " | ");
+				if(j  == 9){
+					System.out.println();
+				}
+			}
+		}
 	}
 }
